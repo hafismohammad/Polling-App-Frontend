443b9342f85bc0065943afa6d10728c10a0b0988
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2025-01-13 12:25:35 +0530

    titile changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserRoute from "./routes/UserRoute";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Import toast styles
+import "react-toastify/dist/ReactToastify.css"; 
 import "./App.css";
 import "./index.css";
 
 function App() {
   return (
     <Router>
-      {/* Your application routes */}
       <Routes>
         <Route path="/*" element={<UserRoute />} />
       </Routes>
 
-      {/* Toast container for displaying notifications */}
       <ToastContainer
-        position="top-right" // You can change the position as needed
-        autoClose={3000} // Duration (in ms) before the toast disappears
-        hideProgressBar={false} // Show or hide the progress bar
-        newestOnTop={false} // Show newest toast on top
-        closeOnClick // Close toast on click
-        rtl={false} // Right-to-left support
-        pauseOnFocusLoss // Pause timer on focus loss
-        draggable // Allow toast dragging
-        pauseOnHover // Pause toast timer on hover
-        theme="light" // Light or dark theme
+        position="top-right" 
+        autoClose={3000} 
+        hideProgressBar={false}
+        newestOnTop={false} 
+        closeOnClick 
+        rtl={false} 
+        pauseOnFocusLoss
+        draggable 
+        pauseOnHover 
+        theme="light" 
       />
     </Router>
   );
